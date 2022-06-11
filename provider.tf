3784e79a500bc4cbda2b3cb7b2234a24517dea63
Andy <94088327+exploitact@users.noreply.github.com>
2022-06-10 18:57:58 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,22 @@
-# Terraform provider that provides access to azurerm
+# 1. Specify the version of the AzureRM Provider to use
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      source = "hashicorp/azurerm"
+      version = "=3.0.1"
     }
   }
-
-  required_version = ">= 0.9.0"
 }
 
+# 2. Configure the AzureRM Provider
 provider "azurerm" {
+  # The AzureRM Provider supports authenticating using via the Azure CLI, a Managed Identity
+  # and a Service Principal. More information on the authentication methods supported by
+  # the AzureRM Provider can be found here:
+  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs#authenticating-to-azure
+
+  # The features block allows changing the behaviour of the Azure Provider, more
+  # information can be found here:
+  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/guides/features-block
   features {}
 }
