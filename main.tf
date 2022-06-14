fc1ded51ee792986c47b0a59da0d3effef8ec9fd
exploitact <forkaiser@gmail.com>
2022-06-14 12:27:42 -0700

    huhuhuasdasd

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-/*
+
 # Sample code for running a docker container
 resource "azurerm_container_group" "rg" {
   name                = var.generic
@@ -36,4 +36,4 @@ resource "azurerm_container_group" "rg" {
     environment = "testing"
   }
 }
-*/
+
